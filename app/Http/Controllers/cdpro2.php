3b8eac0792a6b41c086ab4bbb47716fa214403ce
york <jason945119@gmail.com>
2019-07-27 22:43:23 +0800

    add first route & set docker redis and msql

---
+++ app/Http/Controllers/cdpro2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class cdpro2 extends Controller
+{
+    public function index()
+    {
+        return view('cdpro2');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('cd-pro2', 'cdpro2@index');
