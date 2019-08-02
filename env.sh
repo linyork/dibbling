89e510b5afc0faffd60a226c9456b3da744a5cb1
york <jason945119@gmail.com>
2019-08-02 12:19:39 +0800

    add r

--- env.sh
+++ env.sh
@@ -8,6 +8,7 @@ do
     echo "Choose the option you want:"
     echo "----------------------------------------"
     echo -e "a.\tStart all containers"
+    echo -e "r.\tRestart all containers"
     echo -e "l.\tList all containers"
     echo -e "c.\tClose all containers"
     echo -e "i.\tInto container"
@@ -19,6 +20,11 @@ do
     case $input in
         a)
             # 啟動
+            docker-compose up -d
+            ;;
+        r)
+            # 重啟
+            docker rm -f $(docker ps -a -q) | awk '{print "移除 \""$1"\" Container"}'
             docker-compose up -d
             ;;
         l)
