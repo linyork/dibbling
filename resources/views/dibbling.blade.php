34ac4e66dcee140cac9d688fb4766e1e68e8c68b
york <jason945119@gmail.com>
2019-08-05 10:38:53 +0800

    sample YT player

--- resources/views/dibbling.blade.php
+++ resources/views/dibbling.blade.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+        <script src="{{ asset('js/common/jquery-3.4.1.js')}}"></script>
         <script>
             let ws = new WebSocket('ws://local.dibbling.tw:9502');
 
@@ -16,6 +17,34 @@
         </script>
     </head>
     <body>
-        <h1>Hello</h1>
+    <div id="YouTubeVideoPlayer"></div>
+    <script async src="http://www.youtube.com/iframe_api"></script>
+    <script>
+        function onYouTubeIframeAPIReady() {
+            var player;
+            player = new YT.Player('YouTubeVideoPlayer', {
+                videoId: 'oEeLSBVsrJA',     // YouTube 影片ID
+                width: 560,                 // 播放器寬度 (px)
+                height: 316,                // 播放器高度 (px)
+                playerVars: {
+                    autoplay: 1,            // 在讀取時自動播放影片
+                    controls: 1,            // 在播放器顯示暫停／播放按鈕
+                    showinfo: 0,            // 隱藏影片標題
+                    modestbranding: 1,      // 隱藏YouTube Logo
+                    loop: 0,                // 讓影片循環播放
+                    fs: 0,                  // 隱藏全螢幕按鈕
+                    cc_load_policty: 0,     // 隱藏字幕
+                    iv_load_policy: 3,      // 隱藏影片註解
+                    autohide: 0             // 當播放影片時隱藏影片控制列
+                },
+                events: {
+                    'onReady': function(e) {
+                        console.log(e);
+                        e.target.playVideo();
+                    }
+                }
+            });
+        }
+    </script>
     </body>
 </html>
