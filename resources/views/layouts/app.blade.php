f7c2fdfe674c34e32a574b1f820725281d9abc8f
york <jason945119@gmail.com>
2020-02-05 09:10:44 +0800

    暫時註解

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,9 +78,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('dibbling_record') }}">Record</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('player_controller') }}">Controller</a>
-                            </li>
+{{--                            <li class="nav-item">--}}
+{{--                                <a class="nav-link" href="{{ route('player_controller') }}">Controller</a>--}}
+{{--                            </li>--}}
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
