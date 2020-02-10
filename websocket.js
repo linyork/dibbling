5595be62d81e23887836dd0abe5ce68b8f390edb
york <jason945119@gmail.com>
2020-02-10 16:53:06 +0800

    新增 node log 在 docker 資料夾裡

--- websocket.js
+++ websocket.js
@@ -1,36 +1,31 @@
 #!/usr/bin/env node
-const express = require('express');
-const app = express();
-app.set('trust proxy', true);
-app.get("/", (req, res, next) => {
-    res.end(req.ip);
-});
-
-const server = require('http').Server(app);
+const port = 8443;
+const server = require('http').createServer();
 const io = require('socket.io')(server);
+const log = require('simple-node-logger').createSimpleFileLogger('/var/log/node/node.log');
 
 const playerRoom = io.of('socket/player');
 playerRoom.on('connection',function(socket) {
-    console.log('some one connetion player');
+    log.info('some one connetion player');
 });
 
 const otherRoom = io.of('socket');
 otherRoom.on('connection', (socket) => {
-    console.log('some on connetion');
+    log.info('some on connetion');
 
     // 當有人發送指令時
     socket.on('command', (command) => {
-        console.log(command);
+        log.info('command: '+command);
         playerRoom.emit('command',command);
     });
 
     // 當發生離線事件
     socket.on('disconnect', () => {
-        console.log('some on disconnect');
+        log.info('some on disconnect');
     });
 });
 
 // 注意，這邊的 server 原本是 app
-server.listen(8443, () => {
-    console.log("Server Started. http://localhost:8443");
+server.listen(port, () => {
+    log.info("Server Started. http://localhost:"+port);
 });
