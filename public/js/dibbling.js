ff0fda406bd444ad074c1292bdc558aac4046998
york <jason945119@gmail.com>
2020-02-13 09:09:10 +0800

    新增 socket chat 底層功能

--- public/js/dibbling.js
+++ public/js/dibbling.js
@@ -1,80 +1,79 @@
-$(function() {
-
-    // web socket
-    var socket = io(document.location.protocol+'//'+domain+'/socket');
-    socket.emit('intoDibbling', user.name);
-    socket.on('playing',function (){
-        console.log('change');
-        refresh();
-    });
-
-    // playing
-    function refreshPlaying() {
-        let promise_get_playing = $.ajax({
-            url: 'api/v2/playing',
-            method: "GET"
-        });
-
-        promise_get_playing.done(function (data) {
-            if (data.id) {
-                // have playing
-                $('#playing').text(data.title);
-                $("#playing").attr("href", "https://www.youtube.com/watch?v=" + data.video_id);
-
-            } else {
-                // no playing
-                $('#playing').text('Two Steps From Hell - Victory - YouTube');
-                $("#playing").attr("href", "#");
-            }
-        });
-    }
-
-    // dibbling
-    $('#video-id').keydown(function (e) {
-        if (e.keyCode == 13) {
-            dibbling($('#video-id').val());
-            $("#video-id").val("");
-        }
-    });
-    $(document).on('click', '#dibbling-button', function () {
-        dibbling($('#video-id').val());
-        $("#video-id").val("");
-    });
-
-    function refresh() {
-        refreshPlaying();
-    }
-
-
-    function dibbling(videoId) {
-        if (videoId === '') return;
-        let promise_post_list = $.ajax({
-            url: 'api/v2/list',
-            headers: {
-                'Authorization': 'Bearer ' + $('meta[name="api_token"]').attr('content'),
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
-            method: "POST",
-            dataType: "json",
-            data: {
-                'videoId': videoId,
-            },
-        });
-        promise_post_list.done(SuccessMethod);
-        promise_post_list.fail(FailMethod);
-    }
-
-    function SuccessMethod(e) {
-        if (e['status'] === false) {
-            alert(e.msg);
-        }
-        refresh();
-    }
-
-    function FailMethod(e) {
-        alert(e);
-        console.log(e);
-    }
-
+// web socket
+var socket = io(document.location.protocol + '//' + domain + '/socket');
+socket.emit('intoDibbling', user.name);
+socket.on('playing', function () {
     refresh();
 });
+socket.on('chat', function( chat ) {
+    console.log(chat);
+});
+
+// playing
+function refreshPlaying() {
+    let promise_get_playing = $.ajax({
+        url: 'api/v2/playing',
+        method: "GET"
+    });
+
+    promise_get_playing.done(function (data) {
+        if (data.id) {
+            // have playing
+            $('#playing').text(data.title);
+            $("#playing").attr("href", "https://www.youtube.com/watch?v=" + data.video_id);
+
+        } else {
+            // no playing
+            $('#playing').text('Two Steps From Hell - Victory - YouTube');
+            $("#playing").attr("href", "#");
+        }
+    });
+}
+
+// dibbling
+$('#video-id').keydown(function (e) {
+    if (e.keyCode == 13) {
+        dibbling($('#video-id').val());
+        $("#video-id").val("");
+    }
+});
+$(document).on('click', '#dibbling-button', function () {
+    dibbling($('#video-id').val());
+    $("#video-id").val("");
+});
+
+function refresh() {
+    refreshPlaying();
+}
+
+
+function dibbling(videoId) {
+    if (videoId === '') return;
+    let promise_post_list = $.ajax({
+        url: 'api/v2/list',
+        headers: {
+            'Authorization': 'Bearer ' + $('meta[name="api_token"]').attr('content'),
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        method: "POST",
+        dataType: "json",
+        data: {
+            'videoId': videoId,
+        },
+    });
+    promise_post_list.done(SuccessMethod);
+    promise_post_list.fail(FailMethod);
+}
+
+function SuccessMethod(e) {
+    if (e['status'] === false) {
+        alert(e.msg);
+    }
+    refresh();
+}
+
+function FailMethod(e) {
+    alert(e);
+    console.log(e);
+}
+
+refresh();
--- websocket.js
+++ websocket.js
@@ -4,6 +4,7 @@ const server = require('http').createServer();
 const io = require('socket.io')(server);
 const log = require('simple-node-logger').createSimpleFileLogger('/var/log/node/node.log');
 
+// player connection
 const playerRoom = io.of('socket/player');
 playerRoom.on('connection', function (socket) {
     log.info('some one connetion player.');
@@ -14,27 +15,35 @@ playerRoom.on('connection', function (socket) {
     });
 });
 
+// connection
 const otherRoom = io.of('socket');
 otherRoom.on('connection', (socket) => {
+    // disconnect
+    socket.on('disconnect', () => {
+        log.info('some one disconnect.');
+    });
+
     // log
     log.info('some one connetion.');
     socket.on('intoDibbling', (name) => {
+        socket.name = name;
         log.info(name + ' into dibbling page.');
     });
     socket.on('intoController', (name) => {
+        socket.name = name;
         log.info(name + ' into controller page.');
     });
 
-
-    // 當有人發送指令時
+    // command
     socket.on('command', (command) => {
         log.info('command: ' + command + '.');
         playerRoom.emit('command', command);
     });
 
-    // 當發生離線事件
-    socket.on('disconnect', () => {
-        log.info('some one disconnect.');
+    // chart
+    socket.on('chat', (chat) => {
+       log.info(socket.name + ': ' + chat + '.');
+       playerRoom.emit('chat', chat);
     });
 });
 
