c5eb5e5bc392005bb8d2993ed30b782c58149918
christine <christine@core-tech.tw>
2022-01-11 18:20:08 +0800

    調整css

--- resources/views/common/go_to_top.blade.php
+++ resources/views/common/go_to_top.blade.php
@@ -1,11 +1,4 @@
-<button type="button"  class="go-to-top btn btn-sm btn-outline-secondary css-record-btn"
-        style="position: fixed;
-            bottom: 40px;
-            right: 40px;
-            padding: 0.25rem 0.5rem;
-            font-size: 0.875rem;
-            line-height: 1.5;
-            border-radius: 0.2rem;"
+<button type="button"  class="go-to-top btn btn-outline-secondary"
         onclick="(function() { $('html, body').animate({scrollTop: 0}, 500); })()"
 >
     <i class="fas fa-angle-double-up" style="font-size: xx-large;"></i>
