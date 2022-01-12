2d02a6f41376454c18ba6b1d9808a738dfbc39a1
christine <christine@core-tech.tw>
2022-01-12 14:50:45 +0800

    調整go top呈現時機

--- resources/views/common/go_to_top.blade.php
+++ resources/views/common/go_to_top.blade.php
@@ -1,5 +1,3 @@
-<button type="button"  class="go-to-top btn btn-outline-secondary"
-        onclick="(function() { $('html, body').animate({scrollTop: 0}, 500); })()"
->
+<button type="button" class="go-to-top btn btn-outline-secondary">
     <i class="fas fa-angle-double-up" style="font-size: xx-large;"></i>
 </button>
