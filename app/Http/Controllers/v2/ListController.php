7f1ab819c02141d223b9f114b3c76a2fc9c0f263
york <jason945119@gmail.com>
2022-02-22 17:40:14 +0800

    Revert "測試 extension router name"
    This reverts commit 796897bc3309b59148ab0d5d86f0c0dac20c7a80.

--- app/Http/Controllers/v2/ListController.php
+++ app/Http/Controllers/v2/ListController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Services\ListService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Route;
 
 class ListController extends Controller
 {
@@ -127,7 +126,7 @@ class ListController extends Controller
             $youtubeHelper->parser($videoId);
             if ( $youtubeHelper->getStatus() )
             {
-                if( $this_video = $listService->getSongByVideoId( $youtubeHelper->getVideoId() )->first() && Route::currentRouteName() !== "extension.insert")
+                if( $this_video = $listService->getSongByVideoId( $youtubeHelper->getVideoId() )->first() )
                 {
                     throw new \LogicException( $this_video['title'].PHP_EOL.PHP_EOL."此影片 {$listService->getDibblingUser($this_video['id'])->name} 已經點過，確定要再點一次嗎?" );
                 }
